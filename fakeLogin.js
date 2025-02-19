89f2e7b01febc16b77796752426c0d0624956cfd
Novaro01 <robik2003@gmail.com>
2025-02-19 12:46:33 +0100

    Update fakeLogin.js

--- fakeLogin.js
+++ fakeLogin.js
@@ -1,10 +1,18 @@
 function saveFakeLogin(event) {
     event.preventDefault();
+    
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     
-    localStorage.setItem("fakeUsername", username);
-    localStorage.setItem("fakePassword", password);
+    let data = `Username: ${username}\nPassword: ${password}`;
     
-    alert("Přihlášení uloženo lokálně!");
+    let blob = new Blob([data], { type: "text/plain" });
+    let link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "login_data.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    
+    alert("Přihlášení bylo uloženo do souboru!");
 }
